73c820923d292e42da5d723ae177fb2b265df147
hhxiaoj <554301271@qq.com>
2013-08-07 16:50:36 +0800

    两个列表

--- QCS/src/com/example/qcs/MainActivity.java
+++ QCS/src/com/example/qcs/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.qcs;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button btn01;
+	private Button btn02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        btn01 = (Button)findViewById(R.id.btn01);
+        btn01.setOnClickListener(new OnClickListener(
+        		) {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			
+				
+				startActivity(new Intent(MainActivity.this,TopRecommendedActivity.class));
+			}
+		});
+        btn02 = (Button)findViewById(R.id.btn02);
+        btn02.setOnClickListener(new OnClickListener(
+        		) {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			
+				
+				startActivity(new Intent(MainActivity.this,FilterActivity.class));
+			}
+		});
+        
     }
 
 
